a6b7ab1e943cc288c8c1e2da74e46f4d0043cec4
Czedrick <czedrickb1@learn.cssd.ab.ca>
2018-09-20 14:49:39 -0600

    Final Commit

---
+++ luckysevens/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace luckysevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            Console.WriteLine("I'm going to guess how much sevens you roll");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                    totalSevens++;
+            }
+
+            Console.WriteLine("Next you're going to roll {0} sevens!", totalSevens);
+            Console.WriteLine("(Press enter to continue...)");
+            Console.ReadLine();
+            Console.WriteLine("You rolled {0} sevens", totalSevens);
+
+        }
+    }
+}
